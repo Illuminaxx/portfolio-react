139f5f9a12ed31d1cc4efd19b8562c97ff204410
Illuminaxx <nexgen62@hotmail.fr>
2020-09-18 11:07:39 +0200

    build dist folder

---
+++ build/precache-manifest.f12e9f95370b3f697bce53a2f2351bc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ffae1480819f9ca20aea3ca8fef847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "794d1f7b3509fd3f3587",
+    "url": "/static/css/main.4c06968d.chunk.css"
+  },
+  {
+    "revision": "dab2f5538c8519440ad7",
+    "url": "/static/js/2.8dc96767.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8dc96767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794d1f7b3509fd3f3587",
+    "url": "/static/js/main.6b4dc223.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "2a436f4d58920d7f7bef8e6892c0b5ce",
+    "url": "/static/media/CV.2a436f4d.pdf"
+  }
+]);
