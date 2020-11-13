769688fbbfc745030eae78a374712e6970ace8eb
Illuminaxx <nexgen62@hotmail.fr>
2020-11-13 17:33:53 +0100

    add contact form, replace component structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React, {Component} from 'react';
-import Header from "./components/header/header";
-import About from "./components/about/about";
-import Resume from "./components/resume/resume";
-import Portfolio from "./components/portfolio/portfolio";
-import Contact from "./components/contact/contact";
+import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
-import Footer from "./components/footer/footer";
-import resumeData from "./resumeData";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
 
 class App extends Component {
-   render() {
-     return (
-         <div className="App">
-           <Header resumeData={resumeData} />
-           <About />
-           <Resume />
-           <Portfolio />
-           <Contact />
-           <Footer />
-         </div>
-     );
-   }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        {/*<Testimonials data={this.state.resumeData.testimonials}/>*/}
+        <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/>
+      </div>
+    );
+  }
 }
 
 export default App;
