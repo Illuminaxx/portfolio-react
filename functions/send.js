7c6eec39c3cbbd6da2789f2098cfb60325d1838a
Illuminaxx <nexgen62@hotmail.fr>
2020-11-16 18:52:43 +0100

    modify send functions

--- functions/send.js
+++ functions/send.js
@@ -9,8 +9,8 @@ exports.handler = function(event, context, callback) {
 
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.email,
             pass: process.env.password
