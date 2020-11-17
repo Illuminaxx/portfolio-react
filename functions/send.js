4977c0517afc93d3d1f903909281caa202fc6c0b
Illuminaxx <nexgen62@hotmail.fr>
2020-11-17 09:41:50 +0100

    add accesstoken

--- functions/send.js
+++ functions/send.js
@@ -17,7 +17,8 @@ exports.handler = function(event, context, callback) {
             pass: process.env.password,
             clientId: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken: process.env.ACCESS_TOKEN
         }
     });
 
