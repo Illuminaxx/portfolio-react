11f11959a2499c95e63ed6d9b5f0e5fa413ca81a
Aurélien Hennuyer <nexgen62@hotmail.fr>
2023-03-28 09:01:47 +0000

    add width and height at img

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Aurelien Hennuyer profile pic" />
+            <img className="profile-pic"  src={profilepic} alt="Aurelien Hennuyer profile pic" width={120} height={120} />
          </div>
          <div className="nine columns main-col">
             <h2>A propos</h2>
