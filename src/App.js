f96ea11d1b18aab6b827143152273ad899b1eb17
Aurélien Hennuyer <nexgen62@hotmail.fr>
2023-03-30 09:28:27 +0000

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './components/About';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Portfolio from './components/Portfolio';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import About from './components/About';
+// import Resume from './components/Resume';
+// import Contact from './components/Contact';
+// import Portfolio from './components/Portfolio';
+
+const Header = React.lazy(() => import('./components/Header'));
+const Footer = React.lazy(() => import('./components/Footer'));
+const About = React.lazy(() => import('./components/About'));
+const Resume = React.lazy(() => import('./components/Resume'));
+const Contact = React.lazy(() => import('./components/Contact'));
+const Portfolio = React.lazy(() => import('./components/Portfolio'));
+
 
 class App extends Component {
 
@@ -41,13 +49,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        {/*<Testimonials data={this.state.resumeData.testimonials}/>*/}
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
+        <Suspens fallback={<span>Loading...</span>}>
+          <Header data={this.state.resumeData.main}/>
+          <About data={this.state.resumeData.main}/>
+          <Resume data={this.state.resumeData.resume}/>
+          <Portfolio data={this.state.resumeData.portfolio}/>
+          <Contact data={this.state.resumeData.main}/>
+          <Footer data={this.state.resumeData.main}/>
+        </Suspens>
       </div>
     );
   }
